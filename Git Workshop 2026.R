23aba5a6d88ebad2597710ac967027a5759dbfe3
Rose Myatt <rosemyatt1@gmail.com>
2026-01-20 12:35:03 -0500

    Commit from the repository test

--- Git Workshop 2026.R
+++ Git Workshop 2026.R
@@ -5,3 +5,5 @@ use_git_config(user.name = "Rose Myatt", user.email = "rosemyatt1@gmail.com")
 gitcreds::gitcreds_set()
 
 # Testing to see if I can push to my project 
+
+# Can I pull from this project space 
